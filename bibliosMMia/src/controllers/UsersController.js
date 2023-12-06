1903774eaf5465a47a705714e7ae6859174bee05
Álvaro Miranda <106751440+alvaroomiranda@users.noreply.github.com>
2023-12-06 08:41:18 -0300

    Update UsersController.js

--- bibliosMMia/src/controllers/UsersController.js
+++ bibliosMMia/src/controllers/UsersController.js
@@ -2,58 +2,51 @@ const User = require('../models/User');
 const Book = require('../models/Book');
 
 module.exports = {
-  async createUser(req, res) {
-    const { name, email, password } = req.body;
+  async Usercreate(request, response) {
+    const { name, email, password } = request.body;
 
     const user = await User.create({ name, email, password });
 
-    return res.json(user);
+    return response.json(user);
   },
-  async pageHome(req, res) {
-    return res.render('home');
+  async homeRender(request, response) {
+    return response.render('home');
   },
 
-  async findUser(req, res) {
-    const { id } = req.params;
-    const user = await User.findByPk(id);
+  async findUser(request, response) {
+    const { id } = request.params;
+    const user = await User.findOne({ where: { id: id } });
 
-    res.redirect('/');
+    response.redirect('/');
   },
-  pageLogin(req, res) {
-    res.render('login');
+  loginRender(request, response) {
+    response.render('login');
   },
-  async pageAllBooks(req, res) {
+  async AllBooksrender(request, response) {
     const books = await Book.findAll({ raw: true });
 
     console.log(books);
 
-    res.render('allBooks', { books });
+    response.render('allBooks', { books });
   },
-  async pageRegisterBook(req, res) {
-    res.render('registerBook');
+  async pageRegisterBook(request, response) {
+    response.render('registerBook');
   },
-  async update(req, res) {
-    const { id } = req.params;
-    const { name, email, password } = req.body;
+  async update(request, response) {
+    const { id } = request.params;
+    const { name, email, password } = request.body;
 
     const user = await User.update(
-      {
-        name,
-        email,
-        password,
-      },
-      {
-        where: { id },
-      },
+      { name, email, password },
+      { where: { id }, },
     );
-
-    return res.json({ message: 'Usuario atualizado!' });
+    return response.json({ message: 'Usuario atualizado!' });
   },
 
-  async deleteUser(req, res) {
-    const { id } = req.params;
+  async userDelete(request, response) {
+    const { id } = request.params;
     const user = await User.destroy({ where: { id } });
 
-    return res.json({ message: 'Usuario deletado!' });
+    return response.json({ message: 'Usuario deletado!' });
   },
 };
